d3b3a42c61f1a352d49e9edea5b1ed1750f62e17
Librarius <lloyar@live.com>
2018-11-20 15:59:39 +0800

    Initialization GIT repository

---
+++ Assets/Scripts/PickUp_LightChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp_LightChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Renderer render = GetComponent<Renderer>();
+		render.material.SetFloat("_RimPower", 5*Mathf.Cos(Time.time * 5) + 6);
+	}
+}
